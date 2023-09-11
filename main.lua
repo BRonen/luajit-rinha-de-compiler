aff8f49338fcbbce9591fb5b98ee72d88883b2c3
Brenno Rodrigues <brennopereira6@gmail.com>
2023-09-11 03:26:37 -0300

    added memoization

--- main.lua
+++ main.lua
@@ -21,6 +21,16 @@ function tprint (tbl, indent)
     end
 end
 
+function merge_tables (fst, snd)
+    local result = fst
+    
+    for k,v in pairs(snd) do result[k] = v end
+
+    if(fst.is_pure or snd.is_pure) then result.is_pure = true end
+
+    return result
+end
+
 function get_ast (path)
     local file = io.open(path, "r")
     
@@ -32,7 +42,7 @@ function get_ast (path)
     
     file:close()
     
-    return require("cjson").decode(content).expression;
+    return require("cjson").decode(content);
 end
 
 function map (tbl, f)
@@ -43,6 +53,56 @@ function map (tbl, f)
     end
 
     return t
+end
+
+function check_function_flags (expression, context)
+    if(not expression or type(expression) ~= 'table') then return { is_pure = true } end
+
+    if(expression.kind == 'Print') then
+        return merge_tables(context, { is_pure = false })
+    end
+
+    if(expression.kind == 'If') then
+        local is_recursive = merge_tables(
+            check_function_flags(expression['then'], context),
+            check_function_flags(expression['otherwise'], context)
+        ).is_recursive
+
+        return merge_tables(context, { is_recursive = is_recursive })
+    end
+
+    if(expression.kind == 'Binary') then
+        local is_recursive = merge_tables(
+            check_function_flags(expression.lhs, context),
+            check_function_flags(expression.rhs, context)
+        ).is_recursive
+
+        return merge_tables(context, { is_recursive = is_recursive })
+    end
+    
+    if(expression.kind == 'Call' and expression.callee.text) == context.name then
+        return merge_tables(context, { is_recursive = true })
+    end
+    
+    return check_function_flags(expression.value, context)
+end
+
+--[[
+let fib = fn (n) => {
+  local result = nil
+  
+  while(not n < 2) {
+    result = result + n - 1 + n - 2
+  }
+};
+]]
+function compile_as_iterative_function(expression, context, parameters)
+    local parameters = parameters or ''
+    return 'function ' .. context.name .. ' ( ' .. parameters .. ' )' .. [[
+        local result = nil
+        local stack = {}
+        while(not ]] .. '' .. [[)
+    ]] .. 'end\n'
 end
 
 local compile_binary_operators = {
@@ -92,68 +152,121 @@ local compile_binary_operators = {
     end,
 }
 
+function compile_function_parameters (parameters, delimiter)
+    local delimiter = delimiter or ''
+    local result = ''
+    
+    for k, v in ipairs(parameters) do
+        result = result .. v.text
+        
+        if (k ~= #parameters) then
+            result = result .. delimiter
+        end
+    end
+
+    return result
+end
+
 local compile_expression_by_kind = {
-    Function = function (expression, args)
-        local parameters = ''
+    Function = function (expression, context)
+        local parameters = compile_function_parameters(expression.parameters, ', ')
+        
+        local flags = check_function_flags(expression, context)
+        if(flags.is_pure ~= false) then flags.is_pure = true end
 
-        for k, v in ipairs(expression.parameters) do
-            parameters = parameters .. v.text
-
-            if (k ~= #expression.parameters) then
-                parameters = parameters .. ', '
+        --[[
+            if(is_recursive) then
+                return compile_as_iterative_function(expression, merge_table(context, { is_recursive = true }), parameters)
             end
+        ]]
+
+        local memoization_header = ''
+        
+        if(flags.is_pure) then
+            memoization_header = 'if(call_memoization["' ..
+                context.name .. '" .. ' .. compile_function_parameters(expression.parameters, ' .. "-" .. ') ..
+            ']) then return call_memoization["' .. context.name .. '" .. ' .. compile_function_parameters(expression.parameters, ' .. "-" .. ') .. '] end\n\n'
         end
 
-        return 'function ' .. args.name .. '(' .. parameters .. ')\n' .. compile_expression(expression.value) .. '\nend\n'
+        return 'function ' .. context.name .. '(' .. parameters .. ')\n' ..
+            memoization_header ..
+            compile_expression(
+                expression.value,
+                merge_tables(flags, merge_tables(context, {parameters = expression.parameters}))
+            ) ..
+            '\nend\n\n'
     end,
-    Let = function (expression)
+    Let = function (expression, context)
+        local context = context or {}
         if(expression.value.kind == 'Function') then
-            return compile_expression(expression.value, {name = expression.name.text})
+            return compile_expression(expression.value, merge_tables(context, {name = expression.name.text}))
         end
-        return 'local ' .. expression.name.text .. ' = ' .. compile_expression(expression.value) .. '\n'
+        return 'local ' .. expression.name.text .. ' = ' .. compile_expression(expression.value, merge_tables(context, {is_returning = false})) .. '\n'
     end,
-    If = function (expression)
-        local result = 'if (' .. compile_expression(expression.condition) .. ') then\n return ' .. compile_expression(expression['then'])
+    If = function (expression, context)
+        local result = 'if (' .. compile_expression(expression.condition) .. ') then\n ' ..
+            compile_expression(expression['then'], merge_tables(context, {is_returning = true}))
 
         if(expression.otherwise) then
-            result = result .. '\nelse\n return ' .. compile_expression(expression.otherwise)
+            result = result .. '\nelse\n ' .. compile_expression(expression.otherwise, merge_tables(context, {is_returning = true}))
         end
 
         return result .. ' \nend'
     end,
-    Binary = function (expression)
-        return compile_binary_operators[expression.op](expression)
+    Binary = function (expression, context)
+        local result = compile_binary_operators[expression.op](expression)
+
+        if(context and context.is_pure and context.is_returning) then
+            return 'local result = ' .. result ..
+                '\n call_memoization["' .. context.name .. '" .. ' .. compile_function_parameters(context.parameters, ' .. "-" .. ') .. '] = result\n' .. 
+                'return result'
+        elseif(context and context.is_returning) then
+            return 'return ' .. result
+        end
+        
+        return result
     end,
-    Var = function (expression)
+    Var = function (expression, context)
+        if(context and context.is_returning and context.is_pure) then
+            return '\n call_memoization["' .. context.name .. '" .. ' .. compile_function_parameters(context.parameters, ' .. "-" .. ') .. '] = ' .. expression.text ..
+                '\nreturn ' .. expression.text
+        end
+        if(context and context.is_returning) then return 'return ' .. expression.text end
         return expression.text
     end,
-    Int = function (expression)
+    Int = function (expression, context)
+        if(context and context.is_returning) then return 'return ' .. expression.value end
         return expression.value
     end,
     Call = function (expression)
         local parameters = table.concat(map(expression.arguments, compile_expression), ', ')
         return compile_expression(expression.callee) .. '(' .. parameters .. ')'
     end,
-    Str = function (expression)
+    Str = function (expression, context)
+        if(context and context.is_returning) then return 'return "' .. expression.value .. '"' end
         return '"' .. expression.value .. '"'
     end,
     Print = function (expression)
-        return 'print(' .. compile_expression(expression.value) .. ')'
+        return 'print(' .. compile_expression(expression.value) .. ')\n '
     end,
 }
 
-function compile_expression (expression, args)
-    local next = expression.next and compile_expression(expression.next) or ''
+function compile_expression (expression, context)
+    local next = expression.next and compile_expression(expression.next, context) or ''
 
     local compiler_by_kind = compile_expression_by_kind[expression.kind]
 
     if (compiler_by_kind) then
-        return compiler_by_kind(expression, args) .. next
+        return compiler_by_kind(expression, context) .. next
     end
 
-    return '[]'
+    return '[not implemented]'
 end
 
-local fib_json = get_ast('/var/rinha/source.rinha.json')
+function compile_script (script, context)
+    return 'local call_memoization = {}\n\n' .. compile_expression(script.expression)
+end
 
-print(compile_expression(fib_json))
+local fib_json = get_ast('/var/rinha/fib.json')
+
+print(compile_script(fib_json))
